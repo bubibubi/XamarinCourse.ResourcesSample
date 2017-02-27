7faa18dc6ceef86bb0171d23d0b431646a2e0993
bubibubi <u.ballestrazzi@gmail.com>
2017-02-27 19:55:09 +0100

    Add project files.

---
+++ XamarinResourcesSample/XamarinResourcesSample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinResourcesSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
